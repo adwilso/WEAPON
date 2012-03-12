704a2f3401ca44491675fbe9d7683ca13f3f5d64
Adam Wilson <slauter6@gmail.com>
2012-03-12 18:46:47 -0400

    Added the mainmenu from Steve ready to merge with the timetable function

--- gen/asgard/weapon/R.java
+++ gen/asgard/weapon/R.java
@@ -11,36 +11,68 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
-        public static final int map=0x7f020001;
-        public static final int node=0x7f020002;
-        public static final int seb0=0x7f020003;
-        public static final int seb1=0x7f020004;
-        public static final int seb2=0x7f020005;
-        public static final int seb3=0x7f020006;
-        public static final int serach=0x7f020007;
-        public static final int teb1=0x7f020008;
-        public static final int teb2=0x7f020009;
-        public static final int teb3=0x7f02000a;
-        public static final int teb4=0x7f02000b;
+        public static final int asgard=0x7f020000;
+        public static final int eng_id=0x7f020001;
+        public static final int icon=0x7f020002;
+        public static final int map=0x7f020003;
+        public static final int map_icon=0x7f020004;
+        public static final int map_icon0=0x7f020005;
+        public static final int node=0x7f020006;
+        public static final int seb0=0x7f020007;
+        public static final int seb1=0x7f020008;
+        public static final int seb2=0x7f020009;
+        public static final int seb3=0x7f02000a;
+        public static final int serach=0x7f02000b;
+        public static final int teb1=0x7f02000c;
+        public static final int teb2=0x7f02000d;
+        public static final int teb3=0x7f02000e;
+        public static final int teb4=0x7f02000f;
+        public static final int timetable_icon0=0x7f020010;
+        public static final int ui_back=0x7f020011;
+        public static final int ui_back0=0x7f020012;
     }
     public static final class id {
-        public static final int btnNextMap=0x7f050006;
+        public static final int SimpleUILayout=0x7f050003;
+        public static final int btnNextMap=0x7f050009;
         public static final int button1=0x7f050001;
+        public static final int eng_id=0x7f050005;
+        public static final int gallery=0x7f050004;
         public static final int imageView1=0x7f050000;
         public static final int mapDisplay=0x7f050002;
-        public static final int mapImageView1=0x7f050004;
-        public static final int relativeLayout1=0x7f050003;
-        public static final int zoomControls1=0x7f050005;
+        public static final int mapImageView1=0x7f050007;
+        public static final int relativeLayout1=0x7f050006;
+        public static final int zoomControls1=0x7f050008;
     }
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int mainmap=0x7f030001;
-        public static final int maplayout=0x7f030002;
+        public static final int mainmenu=0x7f030002;
+        public static final int maplayout=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
         public static final int map=0x7f040002;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a HelloGallery.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #HelloGallery_android_galleryItemBackground asgard.weapon:android_galleryItemBackground}</code></td><td></td></tr>
+           </table>
+           @see #HelloGallery_android_galleryItemBackground
+         */
+        public static final int[] HelloGallery = {
+            0x0101004c
+        };
+        /**
+          <p>This symbol is the offset where the {@link asgard.weapon.R.attr#android_galleryItemBackground}
+          attribute's value can be found in the {@link #HelloGallery} array.
+          @attr name android:android_galleryItemBackground
+        */
+        public static final int HelloGallery_android_galleryItemBackground = 0;
+    };
 }
